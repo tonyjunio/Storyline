ff3bc157c212457d0b529fe202833461cd88f920
Anton Junio <antonino.junio@scorch.com.au>
2021-07-20 23:18:17 +0800

    Add project files.

---
+++ Storyline/Models/StoryEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storyline.Models
+{
+    public class StoryEvent
+    {
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public DateTime EventTime { get; set; }
+        public string BannerImage { get; set; }
+        public IEnumerable<string> CharacterTags { get; set; }
+        public IEnumerable<Organization> OrganizationTags { get; set; }
+        public IEnumerable<Virus> VirusTags { get; set; }
+    }
+
+}
---
+++ Storyline/Shared/StoryEvents.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using Storyline.Models;
+using System.Collections.Generic;
+
+namespace Storyline.Shared
+{
+    public class StoryEventsBase : ComponentBase
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter]
+        public IEnumerable<StoryEvent> StoryEvents { get; set; }
+    }
+}
