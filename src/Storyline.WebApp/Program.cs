32342c113f03a8fdb8f44d6561694f23c5b0bc11
Antonino A. Junio <antonino.junio@gmail.com>
2024-07-21 22:47:09 +0800

    Add JSON files as config source

--- src/Storyline.WebApp/Program.cs
+++ src/Storyline.WebApp/Program.cs
@@ -1,9 +1,23 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using System.Reflection;
+
 namespace Storyline.WebApp;
 
 public class Program
 {
+    static string assemblyPath = "";
+    static string environmentName = "";
     public static void Main(string[] args)
     {
+        assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly()?.Location) ?? "";
+        if (assemblyPath is "")
+        {
+            throw new DirectoryNotFoundException(assemblyPath);
+        }
+
+        environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "";
+
         CreateHostBuilder(args).Build().Run();
     }
 
@@ -12,5 +26,10 @@ public class Program
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
+                webBuilder.ConfigureAppConfiguration(c =>
+                    c.SetBasePath(assemblyPath)
+                    .AddJsonFile("appsettings.json", false, true)
+                    .AddJsonFile($"appsettings.{environmentName}.json", true, true)
+                );
             });
 }
